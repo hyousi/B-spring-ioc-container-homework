19d6414292a5f9cb65ce2678f2fabab788c68292
Ze Dang <4649tz@gmail.com>
2020-09-17 20:16:27 +0800

    [zedang] feat: add sol-2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,27 +1,19 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController implements ApplicationContextAware {
+public class GreetingController {
 
-    private ApplicationContext applicationContext;
-
+    @Lookup
     public GreetingService getGreetingService() {
-        return applicationContext.getBean(GreetingService.class);
+        return null;
     }
 
     @GetMapping("/greet")
     public String greet() {
         return getGreetingService().sayHi();
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
     }
 }
